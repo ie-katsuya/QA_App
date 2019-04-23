c123125cfd987476df77beb00468beffd6ae686a
ie-katsuya <k-aoki@i-enter.co.jp>
2019-04-23 13:10:06 +0900

    Q&Aアプリ

---
+++ app/src/main/java/com/example/qa_app/FavoriteActivity.kt
@@ -0,0 +1,31 @@
+package com.example.qa_app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.FloatingActionButton
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.content.Intent
+import android.support.v7.widget.Toolbar
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { _ ->
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            // ログインしていなければログイン画面に遷移させる
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
